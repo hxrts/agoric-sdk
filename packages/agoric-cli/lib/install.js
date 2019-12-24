2215386078a288a9cf3c99462edaa254272540a1
Mark S. Miller <erights@gmail.com>
2019-12-23 22:32:41 -0800

    lint-fix agoric-cli

--- packages/agoric-cli/lib/install.js
+++ packages/agoric-cli/lib/install.js
@@ -5,23 +5,26 @@ import { promises as fs } from 'fs';
 
 export default async function installMain(progname, rawArgs, priv) {
   const { console, error } = priv;
-  const {
-    _: args,
-  } = parseArgs(rawArgs);
+  const { _: args } = parseArgs(rawArgs);
 
-  const pspawn = (...args) => new Promise((resolve, reject) => {
-    const cp = spawn(...args);
-    cp.on('exit', resolve);
-    cp.on('error', () => resolve(-1));
-  });
+  const pspawn = (...args) =>
+    new Promise((resolve, reject) => {
+      const cp = spawn(...args);
+      cp.on('exit', resolve);
+      cp.on('error', () => resolve(-1));
+    });
 
-  const spawnOutput = (cmd, args = [], opts = {}) => new Promise((resolve, reject) => {
-    let buf = '';
-    const cp = spawn(cmd, args, { ...opts, stdio: ['inherit', 'pipe', 'inherit' ] });
-    cp.stdout.on('data', chunk => buf += String(chunk));
-    cp.on('exit', () => resolve(buf));
-    cp.on('error', () => resolve(buf));
-  });
+  const spawnOutput = (cmd, args = [], opts = {}) =>
+    new Promise((resolve, reject) => {
+      let buf = '';
+      const cp = spawn(cmd, args, {
+        ...opts,
+        stdio: ['inherit', 'pipe', 'inherit'],
+      });
+      cp.stdout.on('data', chunk => (buf += String(chunk)));
+      cp.on('exit', () => resolve(buf));
+      cp.on('error', () => resolve(buf));
+    });
 
   // Install Yarn.
   const yarn = `${__dirname}/../node_modules/.bin/yarn`;
@@ -31,7 +34,9 @@ export default async function installMain(progname, rawArgs, priv) {
   // Check for golang version 1.12+.
   const goVsn = await spawnOutput('go', ['version']);
   const match = goVsn.match(/^go version go1\.(\d+)\./);
-  const goCmd = match && Number(match[1]) >= 12 &&
+  const goCmd =
+    match &&
+    Number(match[1]) >= 12 &&
     pspawn('npm', ['install'], { cwd: '.agservers', stdio: 'inherit' });
 
   const iRets = await Promise.all([
@@ -42,7 +47,11 @@ export default async function installMain(progname, rawArgs, priv) {
   ]);
 
   if (!goCmd) {
-    console.log(chalk.bold.yellow(`To run Agoric locally you will need to install Go 1.12+ and rerun '${progname} install'`));
+    console.log(
+      chalk.bold.yellow(
+        `To run Agoric locally you will need to install Go 1.12+ and rerun '${progname} install'`,
+      ),
+    );
   }
   console.log(chalk.bold.green('Done installing'));
 }
--- packages/agoric-cli/lib/start.js
+++ packages/agoric-cli/lib/start.js
@@ -5,18 +5,16 @@ import { promises as fs } from 'fs';
 
 export default async function startMain(progname, rawArgs, priv) {
   const { console, error } = priv;
-  const {
-    reset,
-    _: args,
-  } = parseArgs(rawArgs, {
+  const { reset, _: args } = parseArgs(rawArgs, {
     boolean: ['reset'],
   });
 
-  const pspawn = (...args) => new Promise((resolve, reject) => {
-    const cp = spawn(...args);
-    cp.on('exit', resolve);
-    cp.on('error', () => resolve(-1));
-  });
+  const pspawn = (...args) =>
+    new Promise((resolve, reject) => {
+      const cp = spawn(...args);
+      cp.on('exit', resolve);
+      cp.on('error', () => resolve(-1));
+    });
 
   const exists = async file => {
     try {
@@ -27,8 +25,10 @@ export default async function startMain(progname, rawArgs, priv) {
     }
   };
 
-  if (!await exists('.agservers/node_modules')) {
-    return error(`you must first run '${progname} install' with Go 1.12 or later`);
+  if (!(await exists('.agservers/node_modules'))) {
+    return error(
+      `you must first run '${progname} install' with Go 1.12 or later`,
+    );
   }
 
   if (reset) {
@@ -37,9 +37,9 @@ export default async function startMain(progname, rawArgs, priv) {
   }
 
   // Run scenario3.
-  const css = 'node_modules/\@agoric/cosmic-swingset';
-  if (!await exists('.agservers/solo')) {
-    console.log(chalk.yellow('initializing solo'))
+  const css = 'node_modules/@agoric/cosmic-swingset';
+  if (!(await exists('.agservers/solo'))) {
+    console.log(chalk.yellow('initializing solo'));
     await pspawn(`${css}/bin/ag-solo`, ['init', 'solo', '--egresses=none'], {
       stdio: 'inherit',
       cwd: '.agservers',
@@ -48,10 +48,12 @@ export default async function startMain(progname, rawArgs, priv) {
 
   console.log(chalk.green('linking html directories'));
   const dappHtml = '.agservers/solo/dapp-html';
-  const htmlWallet = '.agservers/solo/html/wallet'; 
+  const htmlWallet = '.agservers/solo/html/wallet';
   // await Promise.all([fs.unlink(dappHtml).catch(() => {}), fs.unlink(htmlWallet).catch(() => {})]);
-  await Promise.all([fs.symlink('../../ui/build', dappHtml).catch(() => {}), 
-    fs.symlink('../../../.agwallet', htmlWallet).catch(() => {})]);
+  await Promise.all([
+    fs.symlink('../../ui/build', dappHtml).catch(() => {}),
+    fs.symlink('../../../.agwallet', htmlWallet).catch(() => {}),
+  ]);
 
   await pspawn(`../${css}/bin/ag-solo`, ['start', '--role=three_client'], {
     stdio: 'inherit',
